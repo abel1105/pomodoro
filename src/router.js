41fd814aa820db0830268cf74a7135ce87c9ad9d
Abel Lee <abel.lee@linecorp.com>
2019-07-16 18:52:18 +0800

    just use process.env.BASE_URL
    https://cli.vuejs.org/zh/guide/mode-and-env.html#%E5%9C%A8%E5%AE%A2%E6%88%B7%E7%AB%AF%E4%BE%A7%E4%BB%A3%E7%A0%81%E4%B8%AD%E4%BD%BF%E7%94%A8%E7%8E%AF%E5%A2%83%E5%8F%98%E9%87%8F

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ import AlarmPanel from '@/views/panels/AlarmPanel';
 Vue.use(Router);
 
 export default new Router({
-  base: process.env.NODE_ENV === 'production' ? '/pomodoro/' : '/',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
